ea2853a1ff57e18e4f89b102dc02bcfe63304a0f
pglibrary80 <pglibrary80@gmail.com>
2024-12-25 20:23:47 +0900

    fix:comment

--- lua/custom/plugins/comment.lua
+++ lua/custom/plugins/comment.lua
@@ -1,9 +1,37 @@
 return {
   'numToStr/Comment.nvim',
-  opts = {
-    pre_hook = require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook(),
-  }
+  event = { 'BufReadPre', 'BufNewFile' },
   dependencies = {
     'JoosepAlviste/nvim-ts-context-commentstring',
   },
+  config = function()
+    local comment = require 'Comment'
+    local prehook = require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook()
+
+    comment.setup {
+      padding = true,
+      sticky = true,
+      ignore = '^$',
+      toggler = {
+        line = 'gcc',
+        block = 'gbc',
+      },
+      opleader = {
+        line = 'gc',
+        block = 'gb',
+      },
+      extra = {
+        above = 'gcO',
+        below = 'gco',
+        eol = 'gcA',
+      },
+      mappings = {
+        basic = true,
+        extra = true,
+        extended = false,
+      },
+      pre_hook = prehook,
+      post_hook = function() end,
+    }
+  end,
 }
