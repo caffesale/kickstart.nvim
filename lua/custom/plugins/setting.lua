5b5b5efd4c9a714ddcc56c2ed8c39a6975da0899
caffesale <pglibrary80@gmail.com>
2025-10-08 21:29:11 +0900

    fix: space

--- lua/custom/plugins/setting.lua
+++ lua/custom/plugins/setting.lua
@@ -11,6 +11,8 @@ vim.opt.smartindent = true
 
 vim.opt.wrap = false
 
+vim.opt.backspace = 'indent,eol,start'
+
 vim.opt.swapfile = false
 vim.opt.backup = false
 vim.opt.undodir = os.getenv 'HOME' .. '/.vim/undodir'
@@ -21,7 +23,6 @@ vim.opt.incsearch = true
 
 vim.opt.termguicolors = true
 
-vim.opt.scrolloff = 8
 vim.opt.signcolumn = 'yes'
 vim.opt.isfname:append '@-@'
 
