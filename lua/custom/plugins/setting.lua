159238f8f867b4c575bdbb1fcd80a6a05877f559
caffesale <pglibrary80@gmail.com>
2025-10-08 21:00:43 +0900

    lazygit.nvim && settings

--- lua/custom/plugins/setting.lua
+++ lua/custom/plugins/setting.lua
@@ -43,3 +43,5 @@ vim.keymap.set('x', '<leader>p', '"_dP')
 vim.keymap.set('n', '<leader>r', [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 
 vim.keymap.set('n', '<leader>x', '<cmd>!chmod +x %<CR>', { silent = true })
+
+return {}
