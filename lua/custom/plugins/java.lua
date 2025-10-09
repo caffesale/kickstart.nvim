ee12a8e9db0574d893b5ff8f5bde93939d6df14f
caffesale <pglibrary80@gmail.com>
2025-10-09 20:38:44 +0900

    fix: jdtls

--- lua/custom/plugins/java.lua
+++ lua/custom/plugins/java.lua
@@ -15,6 +15,25 @@ return {
   },
   ft = { 'java' }, -- Load only for Java files
   config = function()
-    require('java').setup()
+    require('java').setup({
+      jdtls = {
+        settings = {
+          java = {
+            imports = {
+              gradle = {
+                wrapper = {
+                  checksums = {
+                    {
+                      sha256 = "7d3a4ac4de1c32b59bc6a4eb8ecb8e612ccd0cf1ae1e99f66902da64df296172",
+                      allowed = true
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    })
   end,
 }
